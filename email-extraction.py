eaa1d8a7f9375bfd49915c5dd0747af513876b7f
AFvisions <andrew@afvisions.com>
2023-07-02 11:05:25 -0500

    added ability to print folders that do not meet the criteria of coontaining exactly one PDF and provide a count

--- email-extraction.py
+++ email-extraction.py
@@ -1,21 +1,25 @@
 import csv
 import os
 import re
+import pytesseract
 from PIL import Image
 from pdf2image import convert_from_path
-import pytesseract
 
 # Regular expression to match email addresses
 email_regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
 
 # Directory containing your client folders
-root_directory = r"C:\Users\AFuma\Desktop\buford-closed-files-LOCAL\clients\A"  # update with your path
+root_directory = r"C:\Users\AFuma\Desktop\buford-closed-files-LOCAL\clients\A"  # Remember to adjust this path
 
 # Part 1: Get all client last names
 client_last_names = []
 
+# Create a dictionary to store folders that do not meet the criteria of containing exactly one PDF.
+folders_not_one_pdf = {}
+
 # Iterate over all subfolders in the root directory
 for foldername in os.listdir(root_directory):
+    print(f"Processing Folder: {foldername}")
     # Extract the client's last name from the folder name
     client_last_name = foldername.split(',')[0]
     client_last_names.append(client_last_name)
@@ -30,7 +34,6 @@ with open('client_last_names.csv', 'w', newline='') as file:
         writer.writerow([name])
 
 # Part 2: Handle folders with a single PDF
-
 with open('client_emails.csv', 'w', newline='') as file:
     writer = csv.writer(file)
     # Write the header row
@@ -48,20 +51,37 @@ with open('client_emails.csv', 'w', newline='') as file:
             # Only one PDF file - let's handle it
             filename = pdf_files[0]
             full_path = os.path.join(folder_path, filename)
+            
+            print("Starting PDF Extraction for " + filename)
 
-            # Convert the PDF into images
-            images = convert_from_path(full_path)
-
-            # Initialize variable to hold the text data
+            # Initialize variables to hold the text data and extracted information
             text_data = ""
+            emails = []
 
-            # Iterate over the images and perform OCR on each one
-            for i, image in enumerate(images):
-                text_data += pytesseract.image_to_string(image)
+            print("Starting Image Conversion and OCR for " + filename)
+            # Convert PDF to images and perform OCR
+            images = convert_from_path(full_path)
+            for i in range(len(images)):
+                # Perform OCR on the image
+                text = pytesseract.image_to_string(images[i])
+                text_data += text
 
+            print("Starting Email Extraction for " + filename)
             # Use the regex to find all email addresses in the text
             email_matches = re.findall(email_regex, text_data)
-            emails = ', '.join(email_matches) if email_matches else None
+            if email_matches:
+                emails.extend(email_matches)  # Add all found emails to the list
 
             # Write the data to the CSV
-            writer.writerow([client_last_name, filename, emails])
+            for email in emails:
+                writer.writerow([client_last_name, filename, email])
+
+            print("Completed Processing for " + foldername)
+        else:
+            # Not exactly one PDF file in the folder, store it in the dictionary
+            folders_not_one_pdf[foldername] = len(pdf_files)
+
+# Print the folders that do not meet the criteria of containing exactly one PDF.
+print("Folders not fitting the criteria of containing exactly one PDF:")
+for folder, count in folders_not_one_pdf.items():
+    print(f"Folder: {folder}, Number of PDFs: {count}")
