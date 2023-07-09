3ffcf95ac2f4de13a951a5da10011dd0fccd31ca
AFvisions <andrew@afvisions.com>
2023-07-08 21:10:03 -0500

    Version 1.3: Commencing processing of folders A - K. The 'email-extraction' script is designed to traverse through a specified root directory that contains client folders, each holding PDF files. The script processes each PDF file by converting it to an image, and then using OCR (Optical Character Recognition) to extract text data.
    It then uses a regular expression to identify and extract any email addresses found in the OCR'd text. This information is written into a CSV file along with the folder name (client's name), the file name, page number where the email was found, and the status of the process.
    
    In case a folder contains no PDFs or more than one PDF, this is also logged in the CSV under the 'status' column. The script prints progress updates to the console and provides a total processing time at the end.

--- email-extraction.py
+++ email-extraction.py
@@ -12,56 +12,61 @@ email_regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
 # Directory containing your client folders
 root_directory = r"C:\Users\AFuma\Desktop\buford-closed-files-LOCAL\clients\A"  # Remember to adjust this path
 
-# Handle folders with a single PDF
-with open('client_data.csv', 'a', newline='') as file:
+start_time = time.time()
+
+# CSV filename
+csv_filename = 'client_data.csv'
+
+with open(csv_filename, 'a', newline='') as file:
     writer = csv.writer(file)
-    # Write the header row
-    writer.writerow(["ClientLastName", "ClientFirstName", "Filename", "Page", "Email", "Status", "Duration"])
+    if file.tell() == 0:
+        # Write the header row if the file is empty
+        writer.writerow(["FolderName", "Filename", "Email", "PageNumber", "Status"])
 
     # Iterate over all subfolders in the root directory
     for foldername in os.listdir(root_directory):
-        # Extract the client's last name and first name from the folder name
-        names = foldername.split(',')
-        client_last_name = names[0].strip()
-        client_first_name = names[1].strip() if len(names) > 1 else ""
+        print(f"Processing Folder: {foldername}")
 
-        # Check the number of PDF files in the folder
         folder_path = os.path.join(root_directory, foldername)
         pdf_files = [f for f in os.listdir(folder_path) if f.endswith('.pdf')]
-        if len(pdf_files) == 1:
-            # Only one PDF file - let's handle it
-            filename = pdf_files[0]
-            full_path = os.path.join(folder_path, filename)
-            
-            print("Starting PDF Extraction for " + filename)
+        
+        if len(pdf_files) == 0:
+            writer.writerow([foldername, None, None, None, "No PDF"])
+            continue
+        
+        if len(pdf_files) > 1:
+            writer.writerow([foldername, None, None, None, "Multiple PDFs"])
+            continue
+        
+        # Only one PDF file - let's handle it
+        filename = pdf_files[0]
+        full_path = os.path.join(folder_path, filename)
+        
+        print("Starting PDF Extraction for " + filename)
 
-            # Initialize variables to hold the text data and extracted information
-            text_data = ""
-            emails = set()
+        # Initialize variables to hold the text data and extracted information
+        text_data = []
+        emails = set()
 
-            print("Starting Image Conversion and OCR for " + filename)
-            # Convert PDF to images and perform OCR
-            start_time = time.time()
-            images = convert_from_path(full_path)
-            for i in range(len(images)):
-                # Perform OCR on the image
-                text = pytesseract.image_to_string(images[i])
-                text_data += text
+        print("Starting Image Conversion and OCR for " + filename)
+        # Convert PDF to images and perform OCR
+        images = convert_from_path(full_path)
+        for i in range(len(images)):
+            # Perform OCR on the image
+            text = pytesseract.image_to_string(images[i])
+            text_data.append(text)
 
-                # Use the regex to find all email addresses in the text
-                email_matches = re.findall(email_regex, text)
-                if email_matches:
-                    emails.update({(email, i + 1) for email in email_matches})  # Add all found emails to the set
+        print("Starting Email Extraction for " + filename)
+        for i, text in enumerate(text_data):
+            # Use the regex to find all email addresses in the text
+            email_matches = re.findall(email_regex, text)
+            if email_matches:
+                for email in email_matches:
+                    if email not in emails:  # Check if the email is not already recorded
+                        emails.add(email)
+                        writer.writerow([foldername, filename, email, i + 1, "Processed"])  # Add page number and status
 
-            duration = time.time() - start_time
+        print("Completed Processing for " + foldername)
 
-            print("Starting Email Extraction for " + filename)
-            # Write the data to the CSV
-            for email, page in emails:
-                writer.writerow([client_last_name, client_first_name, filename, page, email, "Processed", str(int(duration // 60)) + "m" + str(int(duration % 60)) + "s"])
-
-            print("Completed Processing for " + foldername)
-        elif len(pdf_files) == 0:
-            writer.writerow([client_last_name, client_first_name, "", "", "", "No PDF", ""])
-        else:
-            writer.writerow([client_last_name, client_first_name, "", "", "", "Multiple PDFs", ""])
+elapsed_time = time.time() - start_time
+print(f"Processing Time: {elapsed_time // 3600} hours {(elapsed_time % 3600) // 60} minutes {elapsed_time % 60} seconds")
